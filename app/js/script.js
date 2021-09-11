2a345d211ed64643e51b867b934d425d9148239c
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-09-11 15:01:53 +0300

    Done with the mobile part

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,19 @@
-const shareBtn = document.querySelector('.share__photo');
-const shareBar = document.querySelector('.share__container');
-const shareTriangle = document.querySelector('.share__triangle');
+const shareBtn = document.querySelector(".share__photo");
+const shareBar = document.querySelector(".share__container");
+const shareTriangle = document.querySelector(".share__triangle");
 
-shareBtn.addEventListener('click', () => {
-    if(shareBar.classList.contains('active')){
-        shareBar.style.display = 'none';
-        shareBtn.style.color = 'var(--v-dark-grayish-blue)';
-        shareBtn.style.backgroundColor = 'var(--l-grayish-blue)';
-        shareBar.classList.remove('active');
-        shareTriangle.style.display = 'none';
-    } else {
-        shareBar.style.display = 'flex';
-        shareBtn.style.color = 'white';
-        shareBtn.style.backgroundColor = 'var(--v-dark-grayish-blue)';
-        shareBar.classList.add('active');
-        shareTriangle.style.display = 'block';
-    }
-})
-
+shareBtn.addEventListener("click", () => {
+  if (shareBar.classList.contains("active")) {
+    shareBar.style.display = "none";
+    shareBtn.style.color = "var(--no-active-btn-color)";
+    shareBtn.style.backgroundColor = "var(--no-active-btn-background)";
+    shareBar.classList.remove("active");
+    shareTriangle.style.display = "none";
+  } else {
+    shareBar.style.display = "flex";
+    shareBtn.style.color = "var(--active-btn-color)";
+    shareBtn.style.backgroundColor = "var(--active-btn-background)";
+    shareBar.classList.add("active");
+    shareTriangle.style.display = "block";
+  }
+});
